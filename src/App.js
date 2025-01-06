5982b2d099a8cc5afc2a5c39895940a4f69272c3
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-06 23:10:39 +0530

    Epi - 04 - api call in app component usestate and savr data in usestate hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { filterData } from "./data";
+import React, { useEffect, useState } from "react";
+import { apiUrl, filterData } from "./data";
 import Navbar from "./component/Navbar";
 import Filter from "./component/Filter";
+import Cards from "./component/Cards";
+import { toast } from "react-toastify";
 
 const App = () => {
-  
+   const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const json = res.json();
+        //save data into variable
+        setCourses(json.data);
+      }
+      catch (error) {
+        toast.error("Something went wrong"); 
+      }
+    }
+    fetchData();
+  },[]);
+
   return (
     <div>
       <Navbar />
       <Filter filterData={filterData} />
-      
+      <Cards apiUrl={apiUrl} />
     </div>
   );
 };
