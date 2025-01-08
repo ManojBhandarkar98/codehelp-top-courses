df0a13e50425e5f3b11f6ae5d9d4d9dd082f29fd
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-08 10:02:31 +0530

    Epi - 05 -Navbar and Filter component completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { apiUrl, filterData } from "./data";
-import Navbar from "./component/Navbar";
-import Filter from "./component/Filter";
-import Cards from "./component/Cards";
 import { toast } from "react-toastify";
+import { apiUrl, filterData } from "./data";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
 
 const App = () => {
- 
+
+  const [courses, setCourses] = useState(null);
+  
+  async function fetchData() {
+    try {
+      let response = await fetch(apiUrl);
+      let output = response.json();
+      setCourses(output);
+      
+    }
+    catch (error) {
+      toast.error("Network error")
+    }
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+  console.log(courses);
   return (
     <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Filter filterData={filterData} />
+      </div>
+      <div>
+        <Cards />
+      </div>
+
     </div>
   );
 };
