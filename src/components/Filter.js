964f8cb90002374a2e9a7240107b36dc59c37545
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-09 21:48:45 +0530

    Epi - 05 - Set High lighter on FilterData

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -14,7 +14,10 @@ const Filter = (props) => {
             {
                 filterData.map((data) => {
                     return <button
-                        className={`text-lg px-2 py-1 rounded-md font-medium  text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300`}
+                        className={`text-lg px-2 py-1 rounded-md font-medium  text-white
+                             bg-black hover:bg-opacity-50 border-2 transition-all duration-300
+                             ${category===data.title ? "bg-opacity-60 border-white" : "bg-opacity-40 border-transparent"}
+                             `}
                         key={data.id} onClick={()=> clickFilterHandler(data.title)}>
                             {data.title}</button>
                 })
